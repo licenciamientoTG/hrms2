ea97a397f8e283cbf1b143f6806a7a77fc4d372b
Jose Cardenas <bernardo.cardenas@totalgas.com>
2025-07-11 16:08:18 -0600

    ya se ve tu sueldo de vacaciones

--- apps/vacations/views.py
+++ apps/vacations/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from apps.employee.models import Employee
 
 @login_required
 def vacation_request_view(request):
-    return render(request, 'vacations/user/vacation_form_user.html')
+    try:
+        empleado = Employee.objects.get(user=request.user)
+        vacation_balance = empleado.vacation_balance
+    except Employee.DoesNotExist:
+        vacation_balance = 0  # o None, o lo que prefieras como default
+
+    return render(request, 'vacations/user/vacation_form_user.html', {
+        'vacation_balance': vacation_balance
+    })
