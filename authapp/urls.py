7faffa8ae618975d05af2eaa11a77326ac0bccd6
Jose Cardenas <bernardo.cardenas@totalgas.com>
2025-07-17 10:34:29 -0600

    ya se puede cambiar de contraseñas

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import login_view, register_view, logout_view
 
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
     path("logout/", logout_view, name="logout"),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
