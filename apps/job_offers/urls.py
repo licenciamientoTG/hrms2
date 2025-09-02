7ff117e65559e8ae80c597f7fe8abe04d263c1ca
Jose Cardenas <bernardo.cardenas@totalgas.com>
2025-09-02 11:58:06 -0600

    separecion de job_offers admin y user, creacion de css y js

--- apps/job_offers/urls.py
+++ apps/job_offers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import job_offers_dashboard_view
+from .views import job_offers_dashboard, job_offers_dashboard_admin, job_offers_dashboard_user
 
 urlpatterns = [
-    path('', job_offers_dashboard_view, name='job_offers_dashboard'),
+    path('', job_offers_dashboard, name='job_offers_dashboard'),
+    path('admin/', job_offers_dashboard_admin, name='job_offers_dashboard_admin'),
+    path('user/', job_offers_dashboard_user, name='job_offers_dashboard_user'),
 ]
--- apps/job_offers/views.py
+++ apps/job_offers/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
+# esta vista te redirige a las vistas de usuario y administrador
 @login_required
-def job_offers_dashboard_view(request):
-    return render(request, 'job_offers/job_offers_dashboard.html')
+def job_offers_dashboard(request):
+    if request.user.is_superuser:
+        return redirect('job_offers_dashboard_admin')
+    else:
+        return redirect('job_offers_dashboard_user')
+
+# esta vista es para el administrador
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def job_offers_dashboard_admin(request):
+
+    return render(request, 'job_offers/admin/job_offers_dashboard_admin.html')
+
+# esta vista es para el usuario
+@login_required
+def job_offers_dashboard_user(request):
+
+    return render(request, 'job_offers/user/job_offers_dashboard_user.html')
