2c4878d0e91a823eab8a6c248584b1da55bd0301
Jose Cardenas <bernardo.cardenas@totalgas.com>
2025-09-02 09:42:26 -0600

    separacion de onboarding user y onboardin admin

--- apps/onboarding/urls.py
+++ apps/onboarding/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import onboarding_dashboard_view
+from .views import onboarding_dashboard, onboarding_dashboard_admin, onboarding_dashboard_user
 
 urlpatterns = [
-    path('', onboarding_dashboard_view, name='onboarding_dashboard'),
+    path('', onboarding_dashboard, name='onboarding_dashboard'),
+    path('admin/', onboarding_dashboard_admin, name='onboarding_dashboard_admin'),
+    path('user/', onboarding_dashboard_user, name='onboarding_dashboard_user'),
 ]
--- apps/onboarding/views.py
+++ apps/onboarding/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
+# esta vista te redirige a las vistas de usuario y administrador
 @login_required
-def onboarding_dashboard_view(request):
-    return render(request, 'onboarding/onboarding_dashboard.html')
+def onboarding_dashboard(request):
+    if request.user.is_superuser:
+        return redirect('onboarding_dashboard_admin')
+    else:
+        return redirect('onboarding_dashboard_user')
+
+# esta vista es para el administrador
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def onboarding_dashboard_admin(request):
+
+    return render(request, 'onboarding/admin/onboarding_dashboard_admin.html')
+
+# esta vista es para el usuario
+@login_required
+def onboarding_dashboard_user(request):
+
+    return render(request, 'onboarding/user/onboarding_dashboard_user.html')
